0f98a96b762f8f5fbb72f1ce859565671a92c5a9
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-06-13 16:39:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,27 +1,27 @@
 class Show < ActiveRecord::Base
 
-def self.highest_rating
-    self.maximum(:rating)
-end
+  def self.highest_rating
+      self.maximum(:rating)
+  end
 
-def self.most_popular_show
-  self.highest_rating
-end
+  def self.most_popular_show
+    self.highest_rating
+  end
 
-def self.lowest_rating
-  self.minimum(:rating)
-end
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
 
-def self.least_popular_show
-  self.lowest_rating
-end
+  def self.least_popular_show
+    self.lowest_rating
+  end
 
-def self.popular_shows
-  self.where(:rating > 5)
-end
+  def self.popular_shows
+    self.where(:rating > 5)
+  end
 
-def self.shows_by_alphabetical_order
-  self.where(rating: :desc)
-end
+  def self.shows_by_alphabetical_order
+    self.where(rating: :desc)
+  end
 
 end
